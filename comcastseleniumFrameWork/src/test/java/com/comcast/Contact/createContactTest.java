aa088a2f53c5f37292f9c3719d4541b56ec9ca95
Abhi1234567890123 <abhiraj53443@gmail.com>
2021-09-23 13:24:09 +0530

    uuuu

---
+++ comcastseleniumFrameWork/src/test/java/com/comcast/Contact/createContactTest.java
@@ -0,0 +1,21 @@
+package com.comcast.Contact;
+
+import org.testng.annotations.Test;
+
+public class createContactTest {
+	
+	@Test
+	public void createContactTest() {
+		System.out.println("execute createContactTest");
+		
+	}
+	@Test
+	public void createContactWithOrgTest() {
+		System.out.println("execute createcontactwithOrgTest");
+	}
+	@Test
+	public void deleteContactTest() {
+		System.out.println("execute delete test");
+	}
+
+}
---
+++ comcastseleniumFrameWork/src/test/java/com/comcast/orgtest/copy/OrgTest.java
@@ -0,0 +1,22 @@
+package com.comcast.orgtest.copy;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void createOrganisationTest() {
+		System.out.println("execute createContactTest");
+		
+	}
+	@Test
+	public void creaorgwithcontactTest() {
+		System.out.println("execute createcontactwithOrgTest");
+	}
+	@Test
+	public void deleteorganisationTest() {
+		System.out.println("execute delete test");
+		
+	}
+		
+}
